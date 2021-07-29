994e763f800f6995154de3a10ca7db408c23253f
Sixtus Innocent <sixtusinno@gmail.com>
2021-07-29 14:48:40 +0100

    Added the blog page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 import Resume from "./components/resume";
 import Project from "./components/project";
+import Blog from "./blog/blog";
 
 function App() {
   return (
@@ -11,6 +12,13 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/resume" exact component={Resume} />
         <Route path="/projects" exact component={Project} />
+        <Route
+          path="/blog/posts"
+          exact
+          render={({ history, match }) => (
+            <Blog history={history} basename={match.url} />
+          )}
+        />
       </Switch>
     </div>
   );
---
+++ src/blog/routes/posts/2019-01-05-welcome/post.js
@@ -0,0 +1,8 @@
+const post = {
+  title: `Congratulations on Your New Blog!`,
+  tags: ["react", "navi"],
+  spoiler: "Learn how to add new posts and tweak the theme to your liking.",
+  getContent: () => import("./document.mdx"),
+};
+
+export default post;
